2166ec862d3cde0293bde5d65e9a47560606ada3
ifireice <ifireice@gmail.com>
2012-03-22 22:22:05 +0600

    вторая бомбочка

---
+++ 2/2.c
@@ -0,0 +1,31 @@
+#include <unistd.h>
+#include <stdio.h>
+
+main ()
+{
+	setbuf(stdout, NULL); 
+	p = fork();
+	switch (p)
+	{
+		case -1:
+	   		 printf("Error\n");
+		case 0:
+	    		if(setpgrp()==-1)
+	    		{ 
+				printf("Error");
+				return;
+			}
+			else
+			{	
+				printf("%d\n", getpgrp());   
+		    		while (1)
+		    		{
+					fork();
+					sleep(5);
+		    		}    
+	    		}
+		default:
+			return;
+	}
+	return;
+}
